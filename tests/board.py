8d07385ec4ae918824671c02bbaaa0379770fb29
Marta <mswolinska@gmail.com>
2022-03-24 11:06:11 +0000

    Unit tests for board.py
    Added unit test for: list_available_positions, is_position_available, _select_random_position, update_board.
    Added additional testing condition for count_neighbours function.

--- tests/board.py
+++ tests/board.py
@@ -1,12 +1,17 @@
+import numpy as np
 import pytest
 
-from AlignFive import board
 from AlignFive.board import GameBoard
 from AlignFive.player import Player
-from AlignFive.utils import Position
+from AlignFive.utils import Position, Color
 
-test_array_0 = GameBoard()
-test_array_0.board = [[0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+test_player_1 = Player(1, Color(0, 0, 0))
+test_player_2 = Player(2, Color(255, 255, 255))
+
+test_board_0 = GameBoard()
+test_board_0.board = np.array(
+[
+    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
     [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
     [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
     [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
@@ -25,12 +30,117 @@ test_array_0.board = [[0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0
     [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
     [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
     [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-]
+    ]
+)
+
+test_board_1 = GameBoard()
+test_board_1.board = np.array(
+    [
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+        [2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.],
+    ]
+)
+
+
+test_board_2 = GameBoard()
+test_board_2.board = np.array(
+    [
+        [1, 1, 2, 1, 2, 2, 1, 2, 1, 2, 2, 1, 2, 2, 1, 1, 2, 1, 1],
+        [2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1, 1, 2, 2, 1, 2, 2, 1, 2],
+        [1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 2, 1, 1],
+        [2, 2, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1],
+        [1, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 1, 2, 1, 2, 1, 2, 1, 1],
+        [1, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2],
+        [1, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 1, 2, 2, 1, 2],
+        [2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 1],
+        [1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 2, 2, 2],
+        [1, 1, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1, 1, 1, 2, 1, 1],
+        [1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 1, 1],
+        [2, 1, 2, 1, 2, 2, 1, 1, 1, 2, 1, 1, 2, 2, 2, 2, 1, 2, 2],
+        [1, 1, 1, 2, 2, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1, 2, 1],
+        [1, 2, 2, 1, 2, 2, 1, 1, 2, 2, 2, 1, 1, 2, 2, 1, 1, 1, 1],
+        [2, 2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 2],
+        [1, 2, 2, 2, 1, 2, 1, 2, 2, 1, 1, 2, 1, 1, 2, 2, 1, 1, 2],
+        [2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 1, 1, 1],
+        [1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 2, 2, 1, 2, 2, 1, 1, 2],
+        [2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 2]
+    ]
+)
+
+test_board_small = GameBoard(3)
+test_board_small.board = np.array([[0, 0, 0], [1, 2, 1], [0, 2, 1]])
 
 @pytest.mark.parametrize(["func_input", "expected_value"],
-     [
-         [(test_array_0, Position(4, 3), Position(0, -1), Player(2, (0, 0, 0))), 2],
-     ]
+    [
+        [test_board_small, np.array([True, True, True, False, False, False, True, False, False])],
+    ]
+
+)
+def test_list_available_positions(func_input, expected_value):
+    test_board = func_input
+    test_mask = test_board.list_available_positions()
+
+    assert test_mask.all() == expected_value.all()
+
+
+@pytest.mark.parametrize(["func_input", "expected_value"],
+    [
+        [(test_board_small, Position(0, 0)), True],
+        [(test_board_small, Position(1, 1)), False],
+    ]
+
+)
+def test_is_position_available(func_input, expected_value):
+    test_board, test_move = func_input
+    print(test_board.size)
+    is_available = test_board.is_position_available(test_move)
+
+    assert is_available == expected_value
+
+@pytest.mark.parametrize(["func_input", "expected_value"],
+    [
+        [test_board_small, (0, 2)]
+    ]
+)
+def test_select_random_position(func_input, expected_value):
+    random_index = func_input.select_random_position()
+    assert type(random_index) == type(expected_value)
+
+
+@pytest.mark.parametrize(["func_input", "expected_value"],
+    [
+        [(test_board_small, Position(0, 0), test_player_1), np.array([[1, 0, 0], [1, 2, 1], [0, 2, 1]])],
+        [(test_board_small, Position(2, 0), test_player_2), np.array([[0, 0, 0], [1, 2, 1], [2, 2, 1]])],
+    ]
+ )
+def test_update_board(func_input, expected_value):
+    test_board, test_move, test_player = func_input
+    test_board.update_board(test_move, test_player)
+
+    assert test_board.board.all() == expected_value.all()
+
+@pytest.mark.parametrize(["func_input", "expected_value"],
+    [
+        [(test_board_0, Position(4, 3), Position(0, -1), test_player_2), 2],
+        [(test_board_small, Position(1, 1), Position(-1, -1), test_player_2), 0],
+    ]
 )
 def test_count_neighbours(func_input, expected_value):
     current_board, move, check_dir, curr_player = func_input
